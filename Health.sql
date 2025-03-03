768ea6593ec49307973598a7c197181df8a6a15d
KaviyaNagarajanb4u <kaviyab4u@gmail.com>
2025-03-03 12:37:04 +0530

    Add files via upload

---
+++ Health.sql
@@ -0,0 +1,58 @@
+select * from Health.dbo.ocd_patient_dataset$
+
+SELECT 
+    Gender,
+    COUNT([Patient ID]) AS Patient_count,           
+    round(AVG([Y-BOCS Score (Obsessions)]),2) AS avg_obs_score  
+FROM 
+    Health.dbo.ocd_patient_dataset$
+GROUP BY 
+    Gender                                     
+ORDER BY 
+    Patient_count;                              
+
+SELECT Gender,
+       COUNT(*) AS Patient_Count,
+       round((COUNT(*) * 100.0 / (SELECT COUNT(*) FROM Health.dbo.ocd_patient_dataset$)),2) AS Gender_Percentage
+FROM Health.dbo.ocd_patient_dataset$
+GROUP BY Gender
+ORDER BY Gender;
+
+
+SELECT 
+  Ethnicity,
+  COUNT([Patient ID]) AS patient_Count,
+  round(AVG([Y-BOCS Score (Obsessions)]),2) AS obs_score
+FROM Health.dbo.ocd_patient_dataset$
+GROUP BY Ethnicity
+ORDER BY patient_Count;
+
+ALTER TABLE Health.dbo.ocd_patient_dataset$
+ALTER COLUMN [OCD Diagnosis Date] DATE;
+
+SELECT 
+  FORMAT([OCD Diagnosis Date], 'yyyy-MM-01 00:00:00') AS month,
+  COUNT([Patient ID]) AS patient_count
+FROM Health.dbo.ocd_patient_dataset$
+GROUP BY FORMAT([OCD Diagnosis Date], 'yyyy-MM-01 00:00:00')
+ORDER BY patient_count;
+
+
+SELECT 
+  [Obsession Type],
+  COUNT([Patient ID]) AS Patient_count,
+ round( AVG([Y-BOCS Score (Obsessions)]),2) AS obs_score
+FROM Health.dbo.ocd_patient_dataset$
+GROUP BY [Obsession Type]
+ORDER BY Patient_count;
+
+
+SELECT 
+     [Compulsion Type] ,
+  COUNT([Patient ID]) AS Patient_count,
+ round( AVG([Y-BOCS Score (Obsessions)]),2) AS obs_score
+FROM Health.dbo.ocd_patient_dataset$
+GROUP BY [Compulsion Type]
+ORDER BY Patient_count;
+
+
